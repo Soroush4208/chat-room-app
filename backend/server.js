b399926b2af2c7c399f1333037caaa86c8b5a9bd
soroush4208 <soroush.r80@gmail.com>
2025-07-23 12:31:13 +0330

    Update WebSocket implementation and frontend UI. Upgraded 'ws' dependency to version 8.18.3. Enhanced server to broadcast messages to all connected clients and added input handling in the frontend for sending and displaying messages.

--- backend/server.js
+++ backend/server.js
@@ -5,8 +5,18 @@ const server = http.createServer();
 
 const wss = new websocket.Server({ server });
 
-wss.on('connection', (ws) => {
-  console.log('new client connected');
+wss.on('headers', (headers, req) => {
+  console.log('headers => ', headers);
+});
+
+wss.on('connection', socket => {
+  socket.on('message', message => {
+    wss.clients.forEach(client => {
+      if (client.readyState === websocket.OPEN) {
+        client.send(message.toString());
+      }
+    });
+  });
 });
 
 server.listen(4208, () => {
--- frontend/app.js
+++ frontend/app.js
@@ -1,3 +1,19 @@
 const socket = new WebSocket('ws://localhost:4208');
 
-console.log(socket);
+const inputMessage = document.getElementById('input-message');
+const messages = document.getElementById('messages');
+
+socket.addEventListener('open', () => {
+  inputMessage.addEventListener('keydown', event => {
+    if (event.keyCode === 13) {
+      if (event.target.value.trim()) {
+        socket.send(event.target.value);
+        event.target.value = '';
+      }
+    }
+  });
+
+  socket.addEventListener('message', event => {
+    messages.innerHTML += `<li>${event.data}</li>`;
+  });
+});
